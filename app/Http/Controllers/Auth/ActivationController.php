ea7949bd6dc4b7aac647da75c50611e6c8cf258a
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-08-20 20:37:29 -0600

    Activating and signing in

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
 
 class ActivationController extends Controller
 {
-    public function active()
+    public function activate(Request $request)
     {
+        $user = User::where('email', $request->email)
+                    ->where('activation_token', $request->token)->firstOrFail();
+
+        $user->update([
+            'active'           => true,
+            'activation_token' => null,
+        ]);
+
+        auth()->loginUsingId($user->id);
+
+        return redirect(route('home'))->withSuccess('Activated!  You\'re now signed in.');
     }
 }
